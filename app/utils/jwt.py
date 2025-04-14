aa06910a968259c9cef0dc8f19f5e1c8eb29a29e
Onanta Bassey <onantabasseyvee@gmail.com>
2025-04-14 10:43:48 +0100

    updated jwt.py file

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,10 +1,8 @@
-# 🎨 app/utils/jwt.py
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from app.core.config import SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_MINUTES
 from app.schemas.user import UserOut
 
-# 📅 Set token expiration
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=JWT_EXPIRATION_MINUTES)) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
@@ -13,7 +11,6 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=JWT_ALGORITHM)
     return encoded_jwt
 
-# 🔑 Validate JWT token
 def verify_token(token: str) -> UserOut:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
